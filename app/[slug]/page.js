0dc5823095170ef74c2c6cd45011e4a046dd9eb2
Steven <lrystvn@gmail.com>
2026-06-23 19:43:16 -0500

    use permanentRedirect for slug shortcut

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation'
 
 export default function SlugRedirect({ params }) {
-  redirect(`/drop/${params.slug}`)
+  permanentRedirect(`/drop/${params.slug}`)
 }
